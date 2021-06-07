05793135e145b3e4247af92d33bfc8699203eb2a
Esmeralda22765 <esmelopez14jnc@gmail.com>
2021-06-07 08:33:56 -0500

    Agregando ubicacion actual

--- MapasBELV/app/src/main/java/piwi/esme/dereegresoacasa/Variables.java
+++
@@ -1,9 +0,0 @@
-package piwi.esme.dereegresoacasa;
-import android.app.Application;
-
-public class Variables extends Application {
-    //Creamos las variables de longitud y latitud de tu casita
-    public static double latitud;
-    public static double longitud;
-
-}
